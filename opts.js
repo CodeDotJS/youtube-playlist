9f31a726753c6aadb3949e934da8201b19a5b729
Rishi Giri <rushforlinux@gmail.com>
2018-05-11 14:14:20 +0530

    check api

---
+++ opts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const ytlist = require('./index.js');
+
+const url = 'https://www.youtube.com/playlist?list=PLWKjhJtqVAbnZtkAI3BqcYxKnfWn_C704';
+
+const func = (data, opt) => {
+	return ytlist(data, opt).then(res => {
+		console.log(res.data.playlist);
+	});
+};
+
+func(url, 'id');
+func(url, 'name');
+func(url, 'url');
