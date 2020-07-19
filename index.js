459eb95c05d438ffc42c4716c07a7b4bb94c9cc2
Rishi Giri <rushforlinux@gmail.com>
2020-07-19 21:30:13 +0530

    fixes #14 and #15

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const tag = {
 };
 
 const splitCurrentPlay = str => {
-	return str.indexOf('watch') === -1 ? str : `https://www.youtube.com/playlist?list=${str.split('&list=')[1].split('&t=')[0]}`;
+	return str.indexOf('watch') === -1 ? `${str}&disable_polymer=1` : `https://www.youtube.com/playlist?list=${str.split('&list=')[1].split('&t=')[0]}&disable_polymer=1`;
 };
 
 const isPrivateVideo = tr => {
